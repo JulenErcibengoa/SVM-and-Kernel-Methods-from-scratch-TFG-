e66d0c2f8f2e12697f943f9219cdfb4b7900c555
Julen Ercibengoa PC <julenertzisa@gmail.com>
2024-05-06 19:24:01 +0200

    margin irudi berria sortu eta besteak aktualizatu

--- Grafikoak/2_margina_adibidea.py
+++ Grafikoak/2_margina_adibidea.py
@@ -1,37 +1,72 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
-
+def sortu_puntuak (n_puntu , x_min, x_max, y_min, y_max):
+    kx = []
+    ky = []
+    while len(kx) != n_puntu:
+        x = random.uniform(x_min, x_max)
+        y = random.uniform(y_min, y_max)
+        kx.append(x)
+        ky.append(y)
+    return kx, ky
 
 # Lerroa marrazteko puntuak:
 x = np.linspace(-10, 10, 100)
-y1 = 0.85*x + 0.9
-y2 = 0.6 *x + 0.65
+y1 = x - 0.7
+y2 = x/3 + 0.5
 
+y3 = -0.4 + x - 1
+y4 = x
+
+y5 = -0.4 + 1/3*(x-1)
+y6 = 1.81999 + 1/3*(x-0.26)
 # Grafikoa sortu: 
-plt.plot(x, y1, label='Margin handia')
-plt.plot(x, y2, label = 'Margin txikia')
-plt.xlabel(r'$x_1$')
-plt.ylabel(r'$x_2$',rotation = 0)
+
+
+plt.plot(x, y3, label = 'Margin txikia', linestyle = "dotted", alpha = 1, c = "tab:blue")
+plt.plot(x, y4, label = 'Margin txikia', linestyle = "dotted", alpha = 1, c = "tab:blue")
+plt.plot(x, y5, label = 'Margin txikia', linestyle = "dotted", alpha = 1, c = "tab:orange")
+plt.plot(x, y6, label = 'Margin txikia', linestyle = "dotted", alpha = 1, c = "tab:orange")
+
+
+
+hiper_1, = plt.plot(x, y1, label='Margin txikia', linewidth = 2.2, linestyle = "--")
+hiper_2, = plt.plot(x, y2, label = 'Margin handia', linewidth = 2.2)
+plt.xlabel(r'$x_1$', fontsize = 17)
+plt.ylabel(r'$x_2$',rotation = 0, fontsize = 17)
 plt.tight_layout(pad = 0.2)
 
-plt.grid(True)
-plt.xlim([-3,3])
-plt.ylim([-3,3])
-plt.axhline(0, color='black',linewidth=0.5)
-plt.axvline(0, color='black',linewidth=0.5)
+# plt.grid(True)
+plt.xlim([-3.2,3.2])
+plt.ylim([-3.2,3.2])
+# plt.axhline(0, color='black',linewidth=0.5)
+# plt.axvline(0, color='black',linewidth=0.5)
 plt.gca().set_aspect('equal', adjustable='box')
 
 # Puntuak gehitu
-plt.scatter([2.3, -0.4, -1.4,0.6,1.5], [1.2, -2.3,-1.2,-0.4,-1.5], color='red', marker='_', s = 300)
-plt.scatter([-1.7, -0.4, 0.5,-2.5,-2.6], [0.7, 1.4, 2.6,1.4,-0.3], color='green', marker='+', s = 300)
-plt.scatter([1],[1], color='red', marker='.', s = 300)
+random.seed(124)
+
+n = 15
+# Negatiboak
+kx_minus, ky_minus = sortu_puntuak(n, 1, 3.5, -1, -3.5) 
+plt.scatter(kx_minus, ky_minus, color='red', marker='_', s = 300)
+kx_minus, ky_minus = sortu_puntuak(n-n//3 * 2, 2.2, 3.5, -0.3, -1) 
+plt.scatter(kx_minus, ky_minus, color='red', marker='_', s = 300)
+
+kx_plus, ky_plus = sortu_puntuak(n, -1.4, -3.5, 1.4, 3.5) 
+plt.scatter(kx_plus, ky_plus, color='green', marker='+', s = 300)
+kx_minus, ky_minus = sortu_puntuak(n-n//3 * 2, -0.3, -1,2.2, 3.5) 
+plt.scatter(kx_minus, ky_minus, color='green', marker='+', s = 300)
+
 
 # Distantzien adierazpenak:
-plt.plot([1, 0.6298], [1, 1.43541],linestyle = "--", color='black',linewidth = 2)
-plt.plot([1, 0.8897], [1, 1.1838],linestyle = "--", color='blue',linewidth = 2)
+plt.plot([1, 13/20], [-0.4, -0.05], color='black',linewidth = 2)
+plt.plot([1, 63/100], [-0.4, 71/100], color='black',linewidth = 2)
+plt.scatter([1],[-0.4], color='red', marker='_', s = 500, linewidth = 4)
 
-l =plt.legend(fontsize = 12, loc = 'upper left')
+l =plt.legend(handles = [hiper_1, hiper_2] ,fontsize = 10, loc = 'lower left')
 l.get_frame().set_alpha(1) 
 
 plt.show()
