2cd5f5635fe77ebf065b78199b05f3dd95068b16
Julen Ercibengoa Portatil <julenertzisa@gmail.com>
2024-05-07 18:44:08 +0200

    irudiak hobetu eta modeloen confusion matrix egiten hasi

--- Modelo_hoberena_sortzen/Modeloen_informazioa_adierazi.py
+++ Modelo_hoberena_sortzen/Modeloen_informazioa_adierazi.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import matplotlib.pyplot as plt
 
 oraingo_bidea = os.path.dirname(os.path.realpath(__file__))
 bide_orokorra = os.path.dirname(oraingo_bidea)
@@ -47,9 +48,26 @@ print(f"Behar izan duen denbora: {Nire_gauss_info[0]}")
 print(f"Lortu duen asmatze-proportzioa: {Nire_gauss_info[1]}")
 print()
 
-# Conffusion Matrix egin:
+# # Conffusion Matrix egin:
 
+# Nire_gauss_test_predikzioak = Nire_gauss.predict_anitzkoitza(X_test.values())
 
+# # Gaizki egindako predikzioak bisualizatu:
+digitu_kopurua = 10
+fig, axs = plt.subplots(2,5, figsize = (12,6))
+k = 0
+for i in range(2):
+    for j in range(5):
+        while Nire_gauss.predict(X_test.iloc[k,:]) != Y_test.iloc[i]:
+            k += 1
+        adib = np.array(X_test.iloc[k, :])
+        adib = np.reshape(adib, (28,28))
+        axs[i,j].imshow(adib, cmap = "gray_r")
+        axs[i,j].axis("off")
+        k += 1
+
+plt.tight_layout(pad = 0.2)
+plt.show()
 
 
 
@@ -74,39 +92,39 @@ print()
 
 
 
-# # ----------------------------------------------------------------------------
-# # --------------------SCIKIT MODELOA KERNEL GAUSSIARRA--------------------------
-# # ----------------------------------------------------------------------------
+# # # ----------------------------------------------------------------------------
+# # # --------------------SCIKIT MODELOA KERNEL GAUSSIARRA--------------------------
+# # # ----------------------------------------------------------------------------
 
-# # Modeloa inportatu
-Scikit_gauss = pickle.load(open(os.path.join(bide_orokorra,"Entrenatutako_modeloak","Scikit_modelo_hoberena_gauss.pkl"),"rb"))
-Scikit_gauss_info = pickle.load(open(os.path.join(bide_orokorra,"Entrenatutako_modeloak","Scikit_modelo_hoberena_gauss_info.pkl"),"rb"))
+# # # Modeloa inportatu
+# Scikit_gauss = pickle.load(open(os.path.join(bide_orokorra,"Entrenatutako_modeloak","Scikit_modelo_hoberena_gauss.pkl"),"rb"))
+# Scikit_gauss_info = pickle.load(open(os.path.join(bide_orokorra,"Entrenatutako_modeloak","Scikit_modelo_hoberena_gauss_info.pkl"),"rb"))
 
-# # Informazioa erakutsi:
-print("Nire modeloa, kernel gausiarra:")
-print(f"Behar izan duen denbora: {Scikit_gauss_info[0]}")
-print(f"Lortu duen asmatze-proportzioa: {Scikit_gauss_info[1]}")
-print()
+# # # Informazioa erakutsi:
+# print("Nire modeloa, kernel gausiarra:")
+# print(f"Behar izan duen denbora: {Scikit_gauss_info[0]}")
+# print(f"Lortu duen asmatze-proportzioa: {Scikit_gauss_info[1]}")
+# print()
 
-# Conffusion Matrix egin:
-
-
+# # Conffusion Matrix egin:
 
 
 
 
-# # ----------------------------------------------------------------------------
-# # --------------------SCIKIT MODELOA KERNEL POLINOMIALA-------------------------
-# # ----------------------------------------------------------------------------
 
-# # Modeloa inportatu
-Scikit_poly = pickle.load(open(os.path.join(bide_orokorra,"Entrenatutako_modeloak","Scikit_modelo_hoberena_poly.pkl"),"rb"))
-Scikit_poly_info = pickle.load(open(os.path.join(bide_orokorra,"Entrenatutako_modeloak","Scikit_modelo_hoberena_poly_info.pkl"),"rb"))
 
-# # Informazioa erakutsi:
+# # # ----------------------------------------------------------------------------
+# # # --------------------SCIKIT MODELOA KERNEL POLINOMIALA-------------------------
+# # # ----------------------------------------------------------------------------
 
-print("Nire modeloa, kernel polinomiala:")
-print(f"Behar izan duen denbora: {Scikit_poly_info[0]}")
-print(f"Lortu duen asmatze-proportzioa: {Scikit_poly_info[1]}")
-print()
+# # # Modeloa inportatu
+# Scikit_poly = pickle.load(open(os.path.join(bide_orokorra,"Entrenatutako_modeloak","Scikit_modelo_hoberena_poly.pkl"),"rb"))
+# Scikit_poly_info = pickle.load(open(os.path.join(bide_orokorra,"Entrenatutako_modeloak","Scikit_modelo_hoberena_poly_info.pkl"),"rb"))
+
+# # # Informazioa erakutsi:
+
+# print("Nire modeloa, kernel polinomiala:")
+# print(f"Behar izan duen denbora: {Scikit_poly_info[0]}")
+# print(f"Lortu duen asmatze-proportzioa: {Scikit_poly_info[1]}")
+# print()
 
