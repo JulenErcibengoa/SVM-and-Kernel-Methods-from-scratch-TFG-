73a8788a95fff48b05f9c073a232840fafe4c12a
Julen Ercibengoa Portatil <julenertzisa@gmail.com>
2024-05-07 09:20:29 +0200

    irudiak aldaketa

--- Grafikoak/1_espazio_zatitzaile_adibidea.py
+++ Grafikoak/1_espazio_zatitzaile_adibidea.py
@@ -14,7 +14,7 @@ plt.xlabel(r'$x_1$', fontsize = 17)
 plt.ylabel(r'$x_2$',rotation = 0, fontsize = 17)
 plt.tight_layout(pad = 0.2)
 
-plt.grid(True)
+# plt.grid(True)
 plt.xlim([-3,3])
 plt.ylim([-3,3])
 plt.axhline(0, color='black',linewidth=0.5)
@@ -24,7 +24,7 @@ plt.gca().set_aspect('equal', adjustable='box')
 # Puntuak gehitu
 plt.scatter([2.3, -0.4, -1.4,0.6,1.5], [1.2, -2.3,-1.2,-0.4,-1.5], color='red', marker='_', s = 300)
 plt.scatter([-1.7, -0.4, 0.8,-2.5,-2.6, -1.4], [0.7, 1.4, 2.6,1.4,-0.5, 2.7], color='green', marker='+', s = 300)
-plt.scatter([1],[1], color='red', marker='o', s = 100)
+plt.scatter([1],[1], color='red', marker='_', s = 500, linewidth = 4)
 plt.text(1.1,0.7, r'$(1,1)$', color='black', fontsize=12)
 
 # Bektorea gehitu
