30ffd4c71f7c5f70bb2a7f445956854964dfaaf5
Julen Ercibengoa Portatil <julenertzisa@gmail.com>
2024-05-07 13:47:31 +0200

    irudiak hobetu berriro

--- Grafikoak/1_espazio_zatitzaile_adibidea.py
+++ Grafikoak/1_espazio_zatitzaile_adibidea.py
@@ -25,11 +25,11 @@ plt.gca().set_aspect('equal', adjustable='box')
 plt.scatter([2.3, -0.4, -1.4,0.6,1.5], [1.2, -2.3,-1.2,-0.4,-1.5], color='red', marker='_', s = 300)
 plt.scatter([-1.7, -0.4, 0.8,-2.5,-2.6, -1.4], [0.7, 1.4, 2.6,1.4,-0.5, 2.7], color='green', marker='+', s = 300)
 plt.scatter([1],[1], color='red', marker='_', s = 500, linewidth = 4)
-plt.text(1.1,0.7, r'$(1,1)$', color='black', fontsize=12)
+plt.text(0.8,0.5, r'$(1,1)$', color='black', fontsize=20)
 
 # Bektorea gehitu
 q = plt.quiver(0,0,-1,1,angles = 'xy',scale_units = 'xy', scale = 1, color = 'black')
-plt.text(-1.3,1.1, r'$w$', color='black', fontsize=12)
+plt.text(-1.3,1.1, r'$w$', color='black', fontsize=20)
 
 # l =plt.legend(fontsize = 15, loc = 'upper left')
 # l.get_frame().set_alpha(1) 
